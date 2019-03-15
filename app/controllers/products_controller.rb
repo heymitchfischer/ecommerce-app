5aee0edf83ec80cdd3e5620c6311d442821c998b
Mitch Fischer <mitchfischer6@gmail.com>
2019-03-14 20:54:14 -0500

    Add New and Create actions for Product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,24 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    
+  end
+
+  def create
+    product = Product.new(
+                          name: params[:name],
+                          description: params[:description],
+                          price: params[:price]
+    )
+
+    product.save
+
+    redirect_to "/products/#{product.id}"
+  end
 end
