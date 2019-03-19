919fe4003bab3f0f666bd7b1993312341f5da86e
Mitch Fischer <mitchfischer6@gmail.com>
2019-03-18 21:07:24 -0500

    Add Edit/Update/Destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,4 +22,25 @@ class ProductsController < ApplicationController
 
     redirect_to "/products/#{product.id}"
   end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    product = Product.find(params[:id])
+    product.update(name: params[:name],
+                    description: params[:description],
+                    price: params[:price]
+      )
+
+    redirect_to "/products/#{product.id}"
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+
+    redirect_to "/products"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get '/products/new' => 'products#new'
   post '/products' => 'products#create'
   get '/products/:id' => 'products#show'
+  get '/products/:id/edit' => 'products#edit'
+  put '/products/:id' => 'products#update'
+  delete '/products/:id' => 'products#destroy'
 end
